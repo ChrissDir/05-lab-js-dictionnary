3fb7457b3cb218f98b2999cee1fb9d59ce0a6d6d
ChrissDir <chriss.dirand@laposte.net>
2023-07-24 16:58:13 +0200

    Lundi après-midi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./style.css";
+
+const elements = [
+  document.querySelector('.switch'),
+  document.querySelector('#component'),
+  document.querySelector('#options'),
+  document.querySelector('#prononciation'),
+  document.querySelector('#finder'),
+  document.querySelector('#suggestion'),
+  document.querySelector('#serif'),
+  document.querySelector('#sans-serif'),
+  document.querySelector('#inter'),
+  document.body
+];
+
+const toggleDarkTheme = (isChecked) => {
+  const classAction = isChecked ? 'add' : 'remove';
+  elements.forEach(element => element.classList[classAction]('dark-theme'));
+};
+
+elements[0].addEventListener('click', (e) => {
+  toggleDarkTheme(e.target.checked);
+});
