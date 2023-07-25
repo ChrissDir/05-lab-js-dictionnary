cd60758c9c3127a36439fd4c4d4b5755eaf4870a
ChrissDir <chriss.dirand@laposte.net>
2023-07-25 12:00:31 +0200

    Mardi matin

--- src/index.js
+++ src/index.js
@@ -21,3 +21,19 @@ const toggleDarkTheme = (isChecked) => {
 elements[0].addEventListener('click', (e) => {
   toggleDarkTheme(e.target.checked);
 });
+
+const serif = document.querySelector('#serif');
+const sansserif = document.querySelector('#sans-serif');
+const inter =  document.querySelector('#inter');
+
+serif.addEventListener('click', (e) => {
+  document.body.style.fontFamily = 'serif';
+});
+
+sansserif.addEventListener('click', (e) => {
+  document.body.style.fontFamily = 'sans-serif';
+});
+
+inter.addEventListener('click', (e) => {
+  document.body.style.fontFamily = 'monospace';
+});
