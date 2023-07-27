72bc97e7801bef0f0263605325caa1723b57bc9d
ChrissDir <chriss.dirand@laposte.net>
2023-07-27 16:56:04 +0200

    jeudi après-midi

--- src/index.js
+++ src/index.js
@@ -45,44 +45,61 @@ document.querySelector('#serif').addEventListener('click', () => changeFontFamil
 document.querySelector('#sans-serif').addEventListener('click', () => changeFontFamily('sans-serif'));
 document.querySelector('#inter').addEventListener('click', () => changeFontFamily('monospace'));
 
+let form = document.querySelector('#searchbar');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+});
+
 function search() {
-  let word = document.querySelector('#finder').value;
-  let resultat = document.querySelector('#resultat');
-  let textlist = document.querySelector('ul.texte');
-  let nomdumot = document.querySelector('#nom_mot');
-  let prononciation = document.querySelector('#prononciation');
+  const resultat = document.querySelector('#resultat');
+  const word = document.querySelector('#finder').value.toLowerCase();
 
-  if (word.length != 0) {
-    let url = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word;
-
-    fetch(url).then(response => response.json().then(data => {
-      console.log(data);
-      try {
-
-        for (let words of data) {
-          let definition = words.meanings[0].definitions[0].definition;
-          let syn = words.meanings[0].synonyms[0];
-          let prononcia = words.phonetics[0].text;
-
-          textlist.textContent = definition;
-          synonymous.textContent = syn;
-          nomdumot.textContent = word;
-          prononciation.textContent = "["+ prononcia +"]";
-
-        }
-// mettre suggestion en display none
-      } catch (error) {
-        resultat.textContent = "Aucune définition trouvée !";
-      }
-
-    }));
-
-  }else{
-
+  if (!word) {
     resultat.textContent = "Veuillez remplir ce champ !";
+    return;
+  }
 
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => displayWordData(data))
+    .catch(error => {
+      resultat.textContent = "Aucune définition trouvée !";
+    });
+}
+
+function displayWordData(data) {
+  const word = document.querySelector('#finder').value.toLowerCase();
+  const textLists = Array.from(document.querySelectorAll('ul > li'));
+  const nomdumot = document.querySelector('#nom_mot');
+  const prononciation = document.querySelector('#prononciation');
+  const audioelement = document.querySelector('#audioprono');
+  const synonymous = document.querySelector('#synonymous');
+  const synonymousTitle = document.querySelector('h2 > #synonymous-title');
+
+  for (let words of data) {
+    const { meanings, phonetics } = words;
+    const definitions = meanings.map(meaning => meaning.definitions[0].definition);
+    const syn = meanings[0].synonyms;
+    const prononcia = phonetics[0].text;
+    const audioprononciation = phonetics[0].audio;
+
+    textLists.forEach((textList, index) => {
+      textList.textContent = definitions[index] || '';
+      textList.style.display = definitions[index] ? 'block' : 'none';
+    });
+
+    synonymous.textContent = syn.join(', ');
+    nomdumot.textContent = word;
+    prononciation.textContent = "[" + prononcia + "]";
+    audioelement.src = audioprononciation;
+    audioelement.style.display = audioprononciation ? 'block' : 'none';
+    synonymousTitle.style.display = syn.length > 0 ? 'block' : 'none';
   }
 }
 
 document.querySelector('#loupe-recherche').addEventListener('click', search);
+
+
 
